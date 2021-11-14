086fd914a5ff2a5baef8c97135e3ef2e0eb71c7a
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-14 10:10:15 +0800

    Update simpleddos.py

--- simpleddos.py
+++ simpleddos.py
@@ -50,6 +50,6 @@ if  ans=="1":
         port = port + 1
         print "Sent %s packet to %s throught Port"%(sent,ip)
         if port == 65534:
-        port = 1
+           port = 1
 else:
 SystemExit()
