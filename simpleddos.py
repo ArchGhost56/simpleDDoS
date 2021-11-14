53addcdebcfcb022e9ad05ec7c1c72e1da4642e2
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-14 10:30:40 +0800

    Update simpleddos.py

--- simpleddos.py
+++ simpleddos.py
@@ -42,5 +42,5 @@ while True:
     sent = sent + 1
     port = port + 1
     print "Sent %s packet to %s throught Port"%(sent,ip)
- if port == 65534:
-    port = 1
+     if port == 65534:
+     port = 1
